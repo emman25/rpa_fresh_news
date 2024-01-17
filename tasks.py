87d838d8f978661f7685e850111635e9f165929b
Emmanuel Masomera <emmanuelmasomera@gmail.com>
2024-01-17 13:40:53 +0200

    Remove workitems

--- tasks.py
+++ tasks.py
@@ -455,24 +455,28 @@ class NewsSource:
 
 @task
 def run_news_data_task() -> None:
-    payload = {}
+    payload = {
+    "search_phrase": "computer science",
+    "news_category": "Arts",
+    "num_months": 0
+ }
     
-    try:
-        raise_exception = True
-        for item in workitems.inputs:
-            payload = item.payload
-            if(payload is not None):
-                raise_exception = False
+    # try:
+    #     raise_exception = True
+    #     for item in workitems.inputs:
+    #         payload = item.payload
+    #         if(payload is not None):
+    #             raise_exception = False
         
-        if raise_exception:
-            raise Exception("Load config")
+    #     if raise_exception:
+    #         raise Exception("Load config")
         
-        logger.info(f"Loaded payload from work item: {payload}")
+    #     logger.info(f"Loaded payload from work item: {payload}")
 
-    except Exception as e:
-        with open('configuration.json', 'r') as config_file:
-            payload = json.load(config_file)
-            logger.info(f"Loaded payload from configuration.json: {payload}")
+    # except Exception as e:
+    #     with open('configuration.json', 'r') as config_file:
+    #         payload = json.load(config_file)
+    #         logger.info(f"Loaded payload from configuration.json: {payload}")
 
     newsSource = NewsSource.builder()\
     .with_search_phrase(payload['search_phrase'])\
